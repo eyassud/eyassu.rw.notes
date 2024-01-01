be7c2892f16fd656391b703d8963d2d58b15e5cb
Daniel Eyassu <danieleyassu@hotmail.com>
2024-01-01 17:15:01 -0500

    Auth and models

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,21 @@
 import { Link, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
 
+import { useAuth } from 'src/auth'
+
 const HomePage = () => {
+  const { isAuthenticated, signUp, logOut, logIn } = useAuth()
+
   return (
     <>
       <Metadata title="Home" description="Home page" />
 
       <main>
         <h1>Hello next</h1>
+        <p>{JSON.stringify({ isAuthenticated })}</p>
+        <button onClick={signUp}>sign up</button>
+        <button onClick={logIn}>login</button>
+        <button onClick={logOut}>sign out</button>
       </main>
     </>
   )
